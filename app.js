ed5885cb455ad79d80bac0358f5e298cf1f5e92a
Jenan Mannette <jenanmannette@gmail.com>
2015-08-18 09:36:51 -0600

    small tweaks

--- app.js
+++ app.js
@@ -64,7 +64,6 @@ app.controller('Reddit', function ($scope) {
             comments.push(comment);
             this.person = "";
             this.message = "";
-            this.allComments = 0;
         }
     };
 
